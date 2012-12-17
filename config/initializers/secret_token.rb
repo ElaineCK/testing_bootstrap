536b992296262d380a5b3fb66a390a656697cec0
Elaine Marino & Jessica Goulding <pair+elaine+jessica@commercekitchen.com>
2012-12-17 10:34:16 -0700

    Testing bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestingBootstrap::Application.config.secret_token = '6dd054ff2bcf0e4af38b165ec36bbe4694464574a35560e8ed574f2012a3649bc88208a9d293e40089b3008f9aa3164efd76df3cdda80875fbc1ab5359181074'
